e8c8d0828e25a7d6f8ab2d262abe1e4bace4a62c
Brayan Retamozo <81648333+logartoRS@users.noreply.github.com>
2021-10-08 16:32:06 -0500

    Add files via upload

---
+++ 06 Classes and objects/Animal.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_Classes_and_objects
+{
+	class Animal
+	{
+		// Properties
+		private String name;
+		private String scientificName;
+		private int size;
+		private int weight;
+
+		// Gets and Sets
+		public String Name
+		{
+			get
+			{
+				return name;
+			}
+			set
+			{
+				name = value;
+			}
+		}
+		public String ScientificName
+		{
+			get
+			{
+				return scientificName;
+			}
+			set
+			{
+				scientificName = value;
+			}
+		}
+		public int Size
+		{
+			get
+			{
+				return size;
+			}
+			set
+			{
+				size = value;
+			}
+		}
+		public int Weight
+		{
+			get
+			{
+				return weight;
+			}
+			set
+			{
+				weight = value;
+			}
+		}
+
+		// Default constructor
+		public Animal()
+		{
+			this.name = "Dog";
+			this.scientificName = "Canis familiaris";
+			this.size = 50;
+			this.weight = 20;
+		}
+
+		// Parameterized constructor
+		public Animal(String name, String scientificName, int size, int weight)
+		{
+			this.name = name;
+			this.scientificName = scientificName;
+			this.size = size;
+			this.weight = weight;
+		}
+
+		// Methods
+		public void description()
+		{
+			Console.WriteLine("Name: {0}", this.name);
+			Console.WriteLine("Scientific name: {0}", this.scientificName);
+			Console.WriteLine("Average size: {0} cm", this.size);
+			Console.WriteLine("Average weight: {0} Kg", this.weight);
+		}
+	}
+}
---
+++ 06 Classes and objects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _06_Classes_and_objects
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Use of the class through an object
+			Animal koala = new Animal();
+
+			// Use the Set
+			koala.Name = "Koala";
+			koala.ScientificName = "Phascolarctos cinereus";
+			koala.Size = 70;
+			koala.Weight = 15;
+
+			// Use the constructors
+			Animal dog = new Animal();
+			Animal cat = new Animal("Cat", "Felis catus", 40, 4);
+
+			// Use the Get
+			Console.WriteLine(dog.Name);
+			Console.WriteLine(dog.ScientificName);
+			Console.WriteLine(cat.Name);
+			Console.WriteLine(cat.ScientificName);
+			Console.WriteLine(koala.Name);
+			Console.WriteLine(koala.ScientificName);
+
+			// Use the methods
+			dog.description();
+			cat.description();
+			koala.description();
+
+			Console.ReadLine();
+		}
+	}
+}
