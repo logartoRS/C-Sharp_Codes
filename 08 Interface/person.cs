93637d689fe53cf716d7ea8cbdd0475b1e4c104a
Brayan Retamozo <81648333+logartoRS@users.noreply.github.com>
2021-10-18 13:04:48 -0500

    Update person.cs

--- 08 Interface/person.cs
+++ 08 Interface/person.cs
@@ -6,6 +6,30 @@ namespace _08_Interface
 {
 	interface person
 	{
+		public String Name
+		{
+			get;
+			set;
+		}
+
+		public String LastName
+		{
+			get;
+			set;
+		}
+
+		public String School
+		{
+			get;
+			set;
+		}
+
+		public DateTime DateOfBorn
+		{
+			get;
+			set;
+		}
+		
 		void presentation();
 		void document();
 		int calculateAge();
